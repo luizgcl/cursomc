f9b8a3dcb750dea3a56bcbb8ec3f945f6b7bdf73
Luiz Gustavo <luiizgcl@gmail.com>
2021-11-25 21:53:35 -0300

    Banco de Dados H2 e Criação automatica da base de dados

--- src/main/java/com/luizgcl/cursomc/domain/Categoria.java
+++ src/main/java/com/luizgcl/cursomc/domain/Categoria.java
@@ -3,10 +3,18 @@ package com.luizgcl.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
@@ -49,6 +57,11 @@ public class Categoria implements Serializable {
 			return false;
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome);
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", nome=" + nome + "]";
 	}
 
 }
