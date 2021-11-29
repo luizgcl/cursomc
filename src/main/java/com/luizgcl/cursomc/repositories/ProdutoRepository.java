047cb168a035c8339b6c7b900b3649796a507d27
Luiz Gustavo <luiizgcl@gmail.com>
2021-11-29 14:59:59 -0300

    Produto e associacao n para n

---
+++ src/main/java/com/luizgcl/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.luizgcl.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luizgcl.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{}
