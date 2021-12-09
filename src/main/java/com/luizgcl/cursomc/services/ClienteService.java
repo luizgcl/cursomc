9a319331a22879a0be7056d201b9b19b77a84576
Luiz Gustavo <luiizgcl@gmail.com>
2021-12-09 11:13:09 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/luizgcl/cursomc/services/ClienteService.java
+++ src/main/java/com/luizgcl/cursomc/services/ClienteService.java
@@ -1,9 +1,26 @@
 package com.luizgcl.cursomc.services;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
 import com.luizgcl.cursomc.domain.Cliente;
+import com.luizgcl.cursomc.dto.ClienteDTO;
 
 public interface ClienteService {
 	
 	Cliente find(Integer id);
+	
+	Cliente insert(Cliente obj);
+
+	Cliente update(Cliente obj);
+
+	void delete(Integer id);
+	
+	List<Cliente> findAll();
+	
+	Page<Cliente> findPage(Integer page, Integer linesPerPage, String oderBy, String direction);
+	
+	Cliente fromDto(ClienteDTO objDto);
 
 }
--- src/main/java/com/luizgcl/cursomc/services/impl/ClienteServiceImpl.java
+++ src/main/java/com/luizgcl/cursomc/services/impl/ClienteServiceImpl.java
@@ -1,13 +1,20 @@
 package com.luizgcl.cursomc.services.impl;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.luizgcl.cursomc.domain.Cliente;
+import com.luizgcl.cursomc.dto.ClienteDTO;
 import com.luizgcl.cursomc.repositories.ClienteRepository;
 import com.luizgcl.cursomc.services.ClienteService;
+import com.luizgcl.cursomc.services.exceptions.DataIntegrityException;
 import com.luizgcl.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,6 +28,49 @@ public class ClienteServiceImpl implements ClienteService {
 		Optional<Cliente> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
 				+ ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	@Override
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	@Override
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	@Override
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException exception) {
+			throw new DataIntegrityException("Não é possível excluir um cliente que possui pedidos.");
+		}
+	}
+	
+	@Override
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+	
+	@Override
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest request = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(request);
+	}
+	
+	public Cliente fromDto(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 
 }
